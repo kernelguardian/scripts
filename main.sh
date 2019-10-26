a4bca6d598217985e0ef48f93628fc63a80cb814
Asish <asishbm98@gmail.com>
2019-10-27 01:02:29 +0530

    Fixed some issues

--- main.sh
+++ main.sh
@@ -2,14 +2,20 @@ cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 echo "**ANACONDA DOWNLOAD COMPLETE**"
 bash Anaconda3-2019.03-Linux-x86_64.sh -b
-eval "$(/Users/ubuntu/anaconda/bin/conda shell.bash hook)"
+cd ~
+eval "$(anaconda3/bin/conda shell.bash hook)"
 source ~/.bashrc
 echo "**ANACONDA INSTALLATION COMPLETE**"
 
 jupyter notebook --generate-config
 echo "**\n\nGenerating Jupyter Config file.....**"
+cd ~/.jupyter
+echo "c.NotebookApp.ip = '0.0.0.0'" >> jupyter_notebook_config.py 
+echo "c.NotebookApp.open_browser = False" >> jupyter_notebook_config.py 
+echo "c.NotebookApp.port = 8888" >> jupyter_notebook_config.py 
 
-c.NotebookApp.ip = '0.0.0.0' >> ~/.jupyter/jupyter_notebook_config.py
-c.NotebookApp.open_browser = False >> ~/.jupyter/jupyter_notebook_config.py
-c.NotebookApp.port = <Port Number> >> ~/.jupyter/jupyter_notebook_config.py
+
+
+
+
 
